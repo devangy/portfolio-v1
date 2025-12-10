d88a71734b32ed97377c2643db5e65c32f115a9d
devangnpm <d3vang2002@gmail.com>
2025-12-10 14:23:41 +0530

    fixing image erro

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import react from "././assets/skills/react.svg";
 import postgres from "./assets/skills/postgresql.svg";
 import tailwind from "./assets/skills/tailwindcss-icon.svg";
 import nextjs from "./assets/skills/nextjs-icon.svg";
-import castle from "/castle.webp";
+import castle from "./assets/images/shougan_castle.webp";
 import { Meteors } from "./components/Meteors";
 
 function App() {
@@ -26,8 +26,8 @@ function App() {
 
   return (
     <div
-      className="min-h-screen relative bg-[#162a2a] flex flex-col items-center"
-      style={{ width: "100%", position: "relative" }}
+      className="min-h-screen relative bg-[#162a2a] flex flex-col items-center h-auto"
+      style={{ width: "100%", position: "relative", }}
     >
       <ClickSpark
         sparkColor="#fff"
@@ -36,7 +36,7 @@ function App() {
         sparkCount={8}
         duration={300}
       >
-        <div className="absolute inset-0 z-0 pointer-events-none border-2 ">
+        <div className="absolute inset-0 z-0 pointer-events-none ">
           <Particles
             particleColors={["#ffffff"]}
             particleCount={100}
@@ -62,12 +62,12 @@ function App() {
             className="
     flex lg:flex-row justify-between items-center
     text-yellow-200 w-full max-w-6xl flex-wrap md:justify-between
-  p-5 relative z-10 border-2 mt-12  lg:space-y-12"
+  p-5 relative z-10  mt-12  lg:space-y-12"
             // onDragStart={(e) => e.preventDefault()}
           >
             {/* TEXT */}
 
-            <div className="flex flex-col space-y-8 w-full max-w-md p-2 md:mb-10 lg:-mt-[6em]">
+            <div className="flex flex-col space-y-8 w-full max-w-md p-2 md:mb-10 lg:-mt-[3em]">
               <h1 className="lg:rgb-fade-soft text-4xl sm:text-5xl md:text-7xl -ml-1 lg:text-[5.5rem] font-spacemono leading-loose z-30 text-lime-100 animate rgb-fade-soft">
                 Devang
               </h1>
@@ -83,23 +83,27 @@ function App() {
                   src={github}
                   draggable="false"
                   alt="GitHub"
+                  onClick={() => window.open("https://github.com/devangy", "_blank")}
                 />
 
                 <img
                   className="w-6 sm:w-7 md:w-8  transition duration-400 tilt-wobble-hover"
                   src={linkedin}
                   // draggable="false"
-                  alt="LinkedIn"
-                />
-                <img
-                  className="w-8 sm:w-7 md:w-8 transition tilt-wobble-hover duration-400  -z-50"
-                  src={twitter}
-                  alt="X"
-                />
-                <img
-                  className="w-6 sm:w-7 md:w-8 tilt-wobble-hover transition duration-400 "
-                  src={gmail}
+                    alt="LinkedIn"
+                    onClick={() => window.open("https://www.linkedin.com/in/devangy", "_blank")}
+                  />
+                  <img
+                    className="w-8 sm:w-7 md:w-8 transition tilt-wobble-hover duration-400  -z-50"
+                    src={twitter}
+                    alt="X"
+                    onClick={() => window.open("https://github.com/devangy", "_blank")}
+                  />
+                  <img
+                    className="w-6 sm:w-7 md:w-8 tilt-wobble-hover transition duration-400 "
+                    src={gmail}
                   alt="Gmail"
+                  onClick={() => window.open("mailto:d3vang@gmail.com?subject=Subject&body=Body%20goes%20here", "_blank")}
                 />
               </div>
             </div>
@@ -123,7 +127,7 @@ function App() {
                 chaos={0.1}
                 thickness={2}
                 style={{ borderRadius: 8 }}
-                className="will-change-transform"
+                className="will-change-transform bg-black"
               >
                 <div className="text-slate-200 leading-relaxed font-mono text-xl">
                   <p
@@ -270,8 +274,8 @@ function App() {
               <div className="flex flex-col font-code text-yellow-100 p-2 mt-28">
                 <h3 className="text-4xl font-jetbrains">Experience</h3>
 
-                <div className="flex flex-col bg-slate-950 w-full  mt-8 rounded-xl p-2 font-jetbrains leading-relaxed md: h-auto">
-                  <div className="flex text-white leading-normal text-4xl w-full items-start">
+                <div className="flex flex-col bg-slate-950 w-full  mt-8 rounded-xl p-2 font-jetbrains leading-relaxed md: h-auto border-2">
+                  <div className="flex text-white leading-normal text-4xl w-full items-start ">
                     <div className="flex flex-row w-full items-center border-b border-white pb-2 ">
                       <div className="flex w-full gap-3 text-sky-100">
                         <span className="text-emerald-400 animate-pulse">
@@ -288,7 +292,7 @@ function App() {
                             <SquareArrowOutUpRight
                               size={30}
                               link="https://chatgpt.com"
-                              className="z-50  hover:scale-110 mt-1"
+                              className="z-50  hover:scale-110 mt-1 text-white hover:text-green-300"
                             />
                           </a>
                         </span>
@@ -312,46 +316,83 @@ function App() {
               </div>
 
               {/* PROJECTS */}
-              <div className="relative flex flex-col justify-around border-2 w-full h-[80rem] border-red-400 overflow-hidden p-2 mt-20">
+              <div className="relative flex flex-col  space-y-16  w-full h-[70rem]  overflow-hidden p-2  mt-20">
 
-                <h3 className="text-4xl text-yellow-100 -mt-28 ml-2">Projects</h3>
+                <h3 className="text-4xl text-yellow-100 -mb-10">Projects</h3>
 
-                <div className="flex flex-col space-x-3 w-full h-[22rem] relative overflow-hidden bg-[#121724] rounded-lg  border-2 -mt-56 ">
+                <div className="flex flex-col space-x-3 w-full h-auto relative overflow-hidden justify-around bg-black rounded-lg  border-2 ">
                   <div className="flex flex-row justify-between items-center text-white font-spacemono  w-full  z-10 ">
                     <div className="flex flex-row items-center justify-start p-2 border-b w-full ">
-                      <span className="text-5xl ml-2">
+                      <span className="text-[2.8em] ml-2 text-red-400">
                         Beacon
                         <a
-                          href="https://chatgpt.com"
+                          href="https://github.com/devangy/Beacon"
                           target="_blank"
                           rel="noopener noreferrer"
                           className="inline-block align-middle"
                         >
                           <SquareArrowOutUpRight
                             size={32}
-                            className="z-50 tilt-wobble-hover hover:scale-110 ml-10 -mt-1 hover:text-green-300 "
+                            className="z-50 tilt-wobble-hover hover:scale-110 ml-10 -mt-1 hover:text-green-300 text-white"
+                          />
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex font-jetbrains text-green-300 text-xl p-2 leading-relaxed -ml-10">
+                    Beacon is a real-time chat application I built to explore websockets for bidirectional communication. It uses Socket.io to handle instant messaging with minimal latency.PostgreSQL database to persist message history, ensuring conversations aren&apos;t lost when a user refreshes. Implemented Github OAuth to handle authentication and seamless login. Designed Designed a mobile-first, responsive UI in React, optimizing the chat experience for performance and usability on both desktop and mobile devices.
+                  </div>
+                  <Meteors number={20} />
+                </div>
+
+                <div className="flex flex-col space-x-3 w-full h-auto relative overflow-hidden bg-black rounded-lg  border-2 -mt-56 ">
+
+                  <div className="flex flex-row justify-between items-center text-white font-spacemono  w-full  z-10 ">
+                    <div className="flex flex-row items-center justify-start  text-[2.8em] p-3 border-b w-full ">
+                      <span className="text-5xl ml-2 text-red-400">
+                        DataLog
+                        <a
+                          href="https://github.com/devangy/market"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="inline-block align-middle"
+                        >
+                          <SquareArrowOutUpRight
+                            size={32}
+                            className="z-50 tilt-wobble-hover hover:scale-110 ml-10 -mt-1 hover:text-green-300 text-white "
                           />
                         </a>
                       </span>
                     </div>
                   </div>
                   <div className="flex font-jetbrains text-green-300 text-xl p-2 leading-relaxed">
-                    Beacon is a real-time chat application I built to explore websockets for bidirectional communication. It uses Socket.io to handle instant messaging with minimal latency.PostgreSQL database to persist message history, ensuring conversations aren&apos;t lost when a user refreshes. Implemented Github OAuth to handle authentication and seamless login. Designed Designed a mobile-first, responsive UI in React, optimizing the chat experience for performance and usability on both desktop and mobile devices.
+                    DataLog is a Telegram Bot built with Golang and Telego. It provides notification alerts on new events with volume and whale alerts for tracking whale movements across two prediction markets Kalshi and Polymarket. It uses concurrent API polling system using goroutines, channels, and backoff retry logic to get contracts prices from both markets simultaneously. To prevent duplicates it uses a hashmap and automatically discards duplicates by comparing hashes.
                   </div>
                   <Meteors number={20} />
                 </div>
 
-                <div className="flex w-full h-[12rem] relative overflow-hidden bg-[#121724] rounded-lg p-5 border-2 -mt-48">
-                  <h1 className="text-[3em] z-50 text-white font-spacemono">
-                    DataLog
-                  </h1>
-                  <Meteors number={20} />
-                </div>
-
-                <div className="flex w-full h-[12rem] relative overflow-hidden bg-[#121724] rounded-lg p-5 border-2">
-                  <h1 className="text-[3em] z-50 text-white font-spacemono">
-                    LucidLines
-                  </h1>
+                <div className="flex flex-col space-x-3 w-full h-auto relative overflow-hidden bg-black rounded-lg  border-2 -mt-56 ">
+                  <div className="flex flex-row justify-between items-center text-white font-spacemono  w-full  z-10 ">
+                    <div className="flex flex-row items-center justify-start text-[2.8em] p-3 border-b w-full ">
+                      <span className="text-5xl ml-2 text-red-400">
+                        LucidLines
+                        <a
+                          href="https://github.com/devangy/LucidLines"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="inline-block align-middle"
+                        >
+                          <SquareArrowOutUpRight
+                            size={32}
+                            className="z-50 tilt-wobble-hover hover:scale-110 ml-10 -mt-1 hover:text-green-300 text-white "
+                          />
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex font-jetbrains text-green-300 text-xl p-2 leading-relaxed">
+                  LucidLines is a full-stack blogging platform with powerful admin management tools. I built the backend with Node.js and Express, PostgreSQL and Prisma ORM for queries, focusing on creating clean, efficient RESTful APIs to handle user data and Cloudinary for image uploads. Implemented full CRUD functionality, enabling users to publish, edit, and delete blogs, as well as engage through comments and likes.
+                  </div>
                   <Meteors number={20} />
                 </div>
               </div>
